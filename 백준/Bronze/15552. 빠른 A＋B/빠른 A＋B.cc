6580b621f38d5639bb01d7bb16099cc3d1bfb800
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-02-01 22:01:09 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 240 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+	int T;
+	cin >> T;
+	int a,b;
+	
+	
+	while(T--) {
+		cin >> a >> b;
+		cout << a + b<<'\n';
+	
+	}
+	
+	return 0;
+}
+
+
