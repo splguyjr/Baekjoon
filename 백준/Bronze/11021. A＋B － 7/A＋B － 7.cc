bbaa1c4494d02f5e81bfd2419597dd3a6e5fa883
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-02-01 21:54:41 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	int a,b;
+	
+	int sav;
+	sav = T;
+	
+	while(T--) {
+		cin >> a >> b;
+		cout <<"Case #"<< sav-T<<": "<< a+b << '\n';
+	
+	}
+	
+	return 0;
+}
+
+
