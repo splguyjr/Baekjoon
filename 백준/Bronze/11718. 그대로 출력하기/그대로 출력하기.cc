fee33319e0d892b1937e6d98e99783da3f4f7604
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-02-01 21:57:47 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 1980 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	char a;
+	a = getchar();
+
+	while ( a != -1) {
+		putchar(a);
+		a = getchar();
+	
+		
+		
+	}
+
+	return 0;
+}
