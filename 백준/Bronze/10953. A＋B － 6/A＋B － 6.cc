6e1b1cb3be58a45b6b4cf006e50f28133b4e9298
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-02-01 21:53:53 +0900

    [Bronze II] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	int a,b;
+	char c;
+	while(T--) {
+		cin >> a >> c >> b;
+		cout << a+b << '\n';
+		
+	}
+	
+	return 0;
+}
+
