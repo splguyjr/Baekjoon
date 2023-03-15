732936e6a7f700e80c026caf8fe438f1bea3cfbe
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-03-15 13:50:22 +0900

    [Gold V] Title: 퇴사 2, Time: 844 ms, Memory: 19600 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int day[1500001];
+int pay[1500001];
+int dp[1500001];
+int main() {
+	
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> day[i] >> pay[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+
+
+		dp[i + day[i]] = max(dp[i]+ pay[i], dp[i + day[i]]);
+		dp[i + 1] = max(dp[i], dp[i + 1]);
+
+
+	}
+
+	cout << dp[n + 1];
+}
