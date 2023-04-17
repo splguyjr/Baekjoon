ee93315c15cff146fa076ab2358355ce59365fe1
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-04-17 13:31:31 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int x;
+
+int main() {
+	cin >> x;
+
+	int i = 1;
+
+	while (x > i) {
+		x -= i++;
+	}
+
+	if (i % 2 == 0) {
+		cout << x << "/" << i + 1 - x;
+	}
+
+	else {
+		cout << i + 1 - x << "/" << x;
+	}
+
+
+
+
+	return 0;
+}
