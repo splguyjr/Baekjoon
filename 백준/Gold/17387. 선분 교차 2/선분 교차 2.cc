ef96b74be13f22d65dcd04a769608a3335d0e262
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-05-09 22:41:00 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+struct Point {
+	ll x;
+	ll y;
+	bool operator<=(Point const& p1) {
+		if (x == p1.x) {
+			return y <= p1.y;
+		}
+		return x <= p1.x;
+	}
+};
+
+ll CCW(Point A, Point B, Point C) {
+	ll res = A.x * B.y + B.x * C.y + C.x * A.y - B.x * A.y - C.x * B.y - A.x * C.y;
+
+	if (res > 0) return 1;
+
+	else if (res < 0) return -1;
+
+	return 0;
+}
+
+int main() {
+	Point A, B, C, D;
+
+	cin >> A.x >> A.y >> B.x >> B.y >> C.x >> C.y >> D.x >> D.y;
+
+	ll ABC = CCW(A, B, C);
+	ll ABD = CCW(A, B, D);
+	ll ACD = CCW(A, C, D);
+	ll BCD = CCW(B, C, D);
+
+	ll check1 = ABC * ABD;
+	ll check2 = ACD * BCD;
+
+	//abcd
+	if (check1 <= 0 && check2 <= 0) {
+		if (check1 == 0 && check2 == 0) {
+			if (B <= A) {
+				swap(A, B);
+			}
+			if (D <= C) {
+				swap(C, D);
+			}
+
+			bool flag = true;
+			if (C <= B && A <= D) {
+				cout << 1;
+				flag = false;
+			}
+			if (flag) cout << 0;
+		}
+
+		else cout << 1;
+	}
+
+	else cout << 0;
+
+	return 0;
+}
