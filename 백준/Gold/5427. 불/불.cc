2b2a1d9cafc5968caa1777be6467d9a52da5b28c
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-05-22 01:25:38 +0900

    [Gold IV] Title: 불, Time: 148 ms, Memory: 13808 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.cc
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+int n, m;
+int dx[4] = { -1,0,0,1 };
+int dy[4] = { 0,1,-1,0 };
+
+string arr[1001];
+
+int fire[1001][1001];
+int escape[1001][1001];
+
+queue<pair<int, int>> q1;
+queue<pair<int, int>> q2;
+
+void bfs1() {
+
+	while (!q1.empty()) {
+		int x = q1.front().first;
+		int y = q1.front().second;
+		q1.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+			if (fire[nx][ny] >= 0 || arr[nx][ny] == '#') continue;
+			fire[nx][ny] = fire[x][y] + 1;
+			q1.push({ nx,ny });
+
+		}
+	}
+}
+
+void bfs2() {
+
+	while (!q2.empty()) {
+		int x = q2.front().first;
+		int y = q2.front().second;
+		q2.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+				cout << escape[x][y] + 1 <<endl;
+				return;
+			}
+			if (escape[nx][ny] >= 0 || arr[nx][ny] == '#') continue;
+			if (fire[nx][ny] != -1 && fire[nx][ny] <= escape[x][y] + 1) continue;//불과 겹치지 않게 하는 조건
+			escape[nx][ny] = escape[x][y] + 1;
+			q2.push({ nx,ny });
+
+		}
+	}
+	cout << "IMPOSSIBLE" << endl;
+}
+
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	memset(fire, -1, sizeof(fire));
+	memset(escape, -1, sizeof(escape));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] == '*') {
+				q1.push({ i,j });
+				fire[i][j] = 0;
+			}
+			if (arr[i][j] == '@') {
+				q2.push({ i,j });
+				escape[i][j] = 0;
+			}
+		}
+	}
+
+	bfs1();
+	bfs2();
+
+	q1 = queue<pair<int, int>>();
+	q2 = queue<pair<int, int>>();
+
+	}
+
+	return 0;
+}
