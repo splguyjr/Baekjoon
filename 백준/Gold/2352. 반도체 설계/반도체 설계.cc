ef1742266875a947de1e8eec44d91af8768fa839
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-10-14 23:07:23 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[40001];
+int len, n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        int here;
+        cin >> here;
+        auto pos = lower_bound(arr, arr + len, here);
+        *pos = here;
+        if (pos == arr + len)
+            len++;
+    }
+
+    cout << len;
+    
+    return 0;
+}
