b8278c70c93b35d4088c4f7316f8250a8a519e4f
splguyjr <33226033+splguyjr@users.noreply.github.com>
2023-10-17 00:38:08 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -2,12 +2,14 @@
 
 using namespace std;
 
-long dp[1000001];//1부터 index까지의 구간합을 m으로 나눈 나머지를 저장
-long arr[1001];//0~m-1의 나머지가 각각 몇 번 나왔는지를 저장하는 배열
+int dp[1000001];//1부터 index까지의 구간합을 m으로 나눈 나머지를 저장
+int arr[1001];//0~m-1의 나머지가 각각 몇 번 나왔는지를 저장하는 배열
 int n, m;
 long long ans = 0;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> n >> m;
 
 	int tmp;
@@ -20,7 +22,7 @@ int main() {
 
 	for (int i = 0; i < m; i++) {
 		if (arr[i] >= 2) {
-			ans += (arr[i] * (arr[i] - 1)) / 2;
+			ans += ((long long)arr[i] * (arr[i] - 1)) / 2;
 		}
 	}
 
