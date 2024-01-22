a29f45e41a4d74ea65d3f5da1834a6cfe9a55938
splguyjr <33226033+splguyjr@users.noreply.github.com>
2024-01-22 22:06:55 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << 1999 << '\n';
+
+	for (int i = 0; i < 999; i++) {
+		cout << 1 << " ";
+	}
+
+	for (int i = 0; i < 1000; i++) {
+		cout << 1000 << " ";
+	}
+		
+	return 0;
+}
