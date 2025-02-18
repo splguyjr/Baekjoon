053339351cd373fd1e1f16baaebfbfbf7293c03c
splguyjr <33226033+splguyjr@users.noreply.github.com>
2025-02-18 10:08:49 +0900

    [level 0] Title: 수 조작하기 2, Time: 1202.83 ms, Memory: 388 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        
+        for (int i = 0; i<numLog.length-1; i++) {
+            int a = numLog[i];
+            int b = numLog[i+1];
+            
+            switch (b-a) {
+                case 1:
+                    answer += 'w';
+                    break;
+                case -1:
+                    answer += 's';
+                    break;
+                case 10:
+                    answer += 'd';
+                    break;
+                case -10:
+                    answer += 'a';
+                    break;
+            }
+        }
+        return answer;
+    }
+}
