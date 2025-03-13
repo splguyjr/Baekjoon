db524ccf3a9a0e5bc74e50759bc638f89e238cdb
splguyjr <33226033+splguyjr@users.noreply.github.com>
2025-03-13 09:26:22 +0900

    [Silver III] Title: 주유소, Time: 424 ms, Memory: 40344 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int[] dist;
+    static int[] cityCost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dist = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        cityCost = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int curCost = 1000000001;
+        int ans = 0;
+        // 앞에서부터 해당 도시의 가격 체크하여 최소면 갱신
+        for (int i = 0; i < N - 1; i++) {
+            if (cityCost[i] < curCost) curCost = cityCost[i];
+            ans += curCost * dist[i];
+        }
+
+        System.out.println(ans);
+    }
+}
