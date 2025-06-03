e6452a3347889651b4d3da4d32c2c3a6d2da8999
splguyjr <splguyjr@naver.com>
2025-06-03 23:16:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.28 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,53 +1,28 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int n, int[][] wires) {
-        int minDiff = Integer.MAX_VALUE;
-
-        for (int i = 0; i < wires.length; i++) {
-            // 그래프 초기화
-            List<List<Integer>> graph = new ArrayList<>();
-            for (int j = 0; j <= n; j++) {
-                graph.add(new ArrayList<>());
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                child += dfs(i);
             }
-
-            // i번째 간선은 제거한 상태로 그래프 구성
-            for (int j = 0; j < wires.length; j++) {
-                if (i == j) continue;
-                int v1 = wires[j][0];
-                int v2 = wires[j][1];
-                graph.get(v1).add(v2);
-                graph.get(v2).add(v1);
-            }
-
-            // BFS로 한쪽 서브트리의 크기 측정
-            boolean[] visited = new boolean[n + 1];
-            int count = bfs(1, graph, visited);
-            int diff = Math.abs(count - (n - count));
-
-            minDiff = Math.min(minDiff, diff);
         }
-
-        return minDiff;
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
     }
-
-    private int bfs(int start, List<List<Integer>> graph, boolean[] visited) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
-        int count = 1;
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int next : graph.get(node)) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    queue.add(next);
-                    count++;
-                }
-            }
+    public int solution(int n, int[][] wires) {
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
         }
-
-        return count;
+        dfs(1);
+        return min;
     }
 }
