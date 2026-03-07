bfcfcafb0eec6e7be06a19f93121fe6fe9f65a59
splguyjr <splguyjr@naver.com>
2026-03-08 01:43:53 +0900

    [Bronze III] Title: 정육각형과 삼각형, Time: 96 ms, Memory: 12960 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Math.sqrt(3) * Math.pow(n, 2) / 4);
+    }
+}
