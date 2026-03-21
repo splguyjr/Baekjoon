9e5ba51decd129f8bff3aff19aef716a7e2c9b00
splguyjr <splguyjr@naver.com>
2026-03-22 04:22:05 +0900

    [Bronze IV] Title: 특식 배부, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int a, b, c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(a, n) + Math.min(b, n) + Math.min(c, n));
+    }
+}
