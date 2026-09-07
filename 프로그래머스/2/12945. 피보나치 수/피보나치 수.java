ea17fffbd19f0b41663d8b9b3a0bbd8f2a3b7de6
splguyjr <splguyjr@naver.com>
2026-09-07 14:29:45 +0900

    [level 2] Title: 피보나치 수, Time: 1.98 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int[] fibo;
+    
+    public int solution(int n) {
+        
+        fibo = new int[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for (int i = 2; i<= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        }
+        
+        return fibo[n];
+    }
+}
